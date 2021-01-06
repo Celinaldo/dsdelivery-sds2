1d5ca365c0d4b9edf2b531d3177c637848f39e80
Celinaldo <celinaldoalvesferre@gmail.com>
2021-01-06 20:30:49 -0300

    Project created

---
+++ backend/src/test/java/com/devjuior/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjuior.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
